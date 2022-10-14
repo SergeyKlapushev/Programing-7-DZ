36195392fe75e9218c1b1fc81db11edb29db2945
Sergey Klapushev <klapischeff@yandex.ru>
2022-10-14 13:30:49 +0300

    Task1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B (и в нулевую степень). Использовать свои функции, не использовать Math.Pow */
+
+int a = Number("Введите число: ");
+int b = Degree("Введите степень: ");
+
+Console.WriteLine(Calculation(a, b));
+
+int Number(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int Degree(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int Calculation (int a, int b)
+{
+    int i = 0;
+    int otvet = 1;
+    while(i < b)
+    {  
+        otvet = otvet * a;
+        i++;
+    }
+    return otvet;
+}
