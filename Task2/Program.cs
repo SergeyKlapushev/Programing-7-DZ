d51f263911e2d589201d200ee2c857c0141a1af5
Sergey Klapushev <klapischeff@yandex.ru>
2022-10-14 15:16:58 +0300

    Task2 completed

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Функция должна на вход принимать число, а выдавать сумму его цифр
+
+452 -> 11
+82 -> 10
+9012 -> 12*/ 
+
+int num = Number("Введите число: ");
+int a = 10;
+int b = 0;
+int otvet = 0;
+Console.WriteLine($"Сумма цифр числа {num} = {Calculation(num, a, b, otvet)}");
+
+
+int Number(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int Calculation (int num, int a, int b, int otvet)
+{
+    while(num >= a)
+    {
+        b = (num%a)/(a/10);
+        a *=10;
+        otvet = otvet + b;
+    }
+    b = (num%a)/(a/10);
+    otvet = otvet + b;
+    return otvet;
+}
