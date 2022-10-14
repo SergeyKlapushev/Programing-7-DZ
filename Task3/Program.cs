ff1135411d63575bccc576aed1a70f9237cb3520
Sergey Klapushev <klapischeff@yandex.ru>
2022-10-14 16:21:13 +0300

    Task3 completed

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 3: Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+(По моему выбору, случайное число не будет иметь разрядность выше 2)
+
+1, 2, 5, 7, 19, 6, 7, 8 */ 
+
+Printer(Fill(8));
+
+int[] Fill(int Length)
+{
+    int [] array = new int [Length];
+    for (int i = 0; i <= array.Length-1; i++)
+    {
+        int r = new Random().Next(0, 100);
+        array[i] = r;
+    }
+    return array;
+}
+
+
+void Printer(int [] array)
+{
+    for (int i = 0; i <= array.Length-1; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
